d095c49833b443132bccf5cbb4c8a3e9a96b9e54
Daniel Alejandro <61702528+danielalejandr0@users.noreply.github.com>
2020-09-27 00:14:10 -0400

    Committ
    fixed local DB connection problem

--- Beta/index.js
+++ Beta/index.js
@@ -20,27 +20,10 @@ const PtSearchSchema = new mongoose.Schema({
     city: String, 
     state: String, 
     zip: String,
-
 });
  
 const PtSearch = mongoose.model("PtSearch", PtSearchSchema);
-
-// const search1 = new PtSearch ({
-//     fName: "Daniel",
-//     lName: "Tosta",
-//     dob: "12-18-1989",
-//     address1: "1234 Main St.",
-//     address2: "null",
-//     city: "gainesville",
-//     state: "Georgia",
-//     zip: "30504",
-// });
-
-// search1.save(function (err, PtSearch) {
-//     if (err) return console.error(err);
-//     console.log(PtSearch.name + " saved to Pt collection.");
-// });
-
+ 
 // ASK Mike why it has to be above home
 app.get("/register", function (req, res) {
     console.log(req);
@@ -52,30 +35,26 @@ app.get("/", function (req, res){
     res.render("app")
 });
 
-app.post("/", function (req, res){
-    console.log(req.body.FName);
-}); 
 
-// app.post("/", function (req, res){ 
-//     const userSearch = new PtSearch({
-//         fName: "req.body.FName",
-//         lName: "req.body.LName",
-//         dob: "req.body.DOB",
-//         address1: "req.body.address1",
-//         address2: "req.body.address2",
-//         city: "req.body.city",
-//         state: "req.body.state",
-//         zip: "req.body.zip",
-//     })
-//     userSearch.save(function (err) {
-//         if (err) {
-//             return console.error(err);
-//             console.log(" saved to PtSearch collection.");
-//         };
-//     });
-// });
-
- 
+app.post("/", function (req, res){ 
+    const userSearch = new PtSearch({
+        fName: req.body.FName,
+        lName: req.body.LName,
+        dob: req.body.DOB,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+    }); 
+    userSearch.save(function (err, PtSearch) {
+        if (err) 
+            { return console.error(err);
+            } else {
+            console.log(PtSearch.name + " saved to Pt collection.");
+            }; 
+    });
+});
 
 app.listen(3000, function(){
     console.log("listening on 3000")
